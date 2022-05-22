993b6b7633475eebf5600a3e6302a1326ccb7168
Simon Levey <lev_47@hotmail.com>
2022-05-22 23:33:15 +0800

    added get, post, delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
      // be sure to include its associated Products
     include: [
       {
-        model: Product
+        model: Product,
       }
     ]
   }).then((categories) => {
@@ -20,19 +20,53 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+      }
+    ]
+  }).then((category) => {
+    res.json(category);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    name: req.body.name
+  }).then((created) => {
+    res.json(created);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    name: req.body.name
+  }, {
+    where: {
+      id: req.params.id,
+    }
+  }).then((updated) => {
+    res.json(updated);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((result) => {
+
+    res.json({
+      data: 'ok'
+    })
+
+  })
 });
 
 module.exports = router;
